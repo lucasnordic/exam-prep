146a77e9132e5c6b14dedd6fa6c92afe0b9f651d
Lucas Nordgren <gusnordglu@student.gu.se>
2022-03-14 13:58:27 +0100

    ex 9 vowels etc done

--- ex8.c
+++ ex8.c
@@ -1,33 +1,47 @@
 //
-// Created by lucas on 2022-03-10.
-// Exercise ?_?
+// Created by lucas on 2022-03-14.
+// Exercise 8
+//
+// Write a program in C to count the number of vowels and consonants in a string using a pointer.
 
+// Includes
 #include <stdio.h>
-#define MAX_SIZE 5 //Maximum size of the array
-int main(){
-    int arr[MAX_SIZE]; //Declares an array size
-    int i, num;
-    int a[5] = {1,2,3,4,5};
-    printf("%c", a[2]);
-    //Enter size of array
-    printf("Enter size of the array: ");
-    scanf("%d", &num);
-    //Reading elements of array
-    printf("Enter elements in array: ");
-    for(i=0; i<num; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    printf("\nAll negative elements in array are: ");
+#include <stdlib.h>
+#include <string.h>
 
-    for(i=0; i<num; ++i)
-    {
-        //Printing negative elements
-        if(arr[i] < 0)
+// Defines
+#define MAX_INPUT 100
+#define INPUT_Q "Input a string: "
+#define RESULT_PRINT_1 "Number of vowels: "
+#define RESULT_PRINT_2 "Number of consonants: "
+
+// Constants
+//char const string[] = "Message: Hello, this is world speaking!\n Response: Hello world!\n";
+
+int main(){
+    char *input = malloc(MAX_INPUT);
+    int inputLength = 0;
+    int vowels = 0;
+    int consonants = 0;
+
+    printf("%s", INPUT_Q);
+    fgets(input, MAX_INPUT, stdin);
+    inputLength = strlen(input) - 1;
+
+    for (int i = 0; i < inputLength; ++i) {
+        if(input[i] == 'a' || input[i] == 'o' || input[i] == 'e'
+           || input[i] == 'i' || input[i] == 'A' || input[i] == 'O' || input[i] == 'E'
+           || input[i] == 'I')
         {
-            printf("%d\t", arr[i]);
+            vowels++;
+        } else if (input[i] != '\n' && input[i] != ' ' ) {
+            consonants++;
         }
     }
+
+    printf("%s%d\n%s%d",RESULT_PRINT_1, vowels, RESULT_PRINT_2, consonants);
+
+    free(input);
 
     return 0;
 }
