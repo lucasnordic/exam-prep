d9fdf2eab3f14476dac9adc6c41e56de87f5c655
Lucas Nordgren <gusnordglu@student.gu.se>
2022-03-14 15:41:47 +0100

    ex 10 "toggle bit" - done

--- exa.c
+++ exa.c
@@ -6,10 +6,28 @@
 // blabla
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 int main(){
+    setbuf(stdout, 0); // enable console prints while debugging
 
-    setbuf(stdout, 0);
+    uint32_t *inputAsInt = malloc(sizeof(uint32_t));
+    int *input2AsInt = malloc(sizeof(int));
+
+    printf("Input number:");
+    scanf("%d", inputAsInt);
+
+    printf("Input nth bit to toggle:");
+    scanf("%d", input2AsInt);
+
+    uint32_t mask = 1 << ((*input2AsInt) - 1);
+    *inputAsInt = *inputAsInt ^ mask;
+
+    printf("After toggling bit %d: %d\n", *input2AsInt, *inputAsInt);
+
+    free(inputAsInt);
+    free(input2AsInt);
 
     return 0;
 }
